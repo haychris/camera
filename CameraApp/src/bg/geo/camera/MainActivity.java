28ae7b2a0718fad608723fcf5d495b4fb07dce67
GTachev <tachev@geo.bg>
2014-03-29 13:07:06 -0400

    Camera

--- CameraApp/src/bg/geo/camera/MainActivity.java
+++ CameraApp/src/bg/geo/camera/MainActivity.java
@@ -1,15 +1,43 @@
 package bg.geo.camera;
 
+import java.io.IOException;
+
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements IPictureTakenListener{
+	
+	private Camera camera1; 
+	private Camera camera2;
+	private boolean pictureTaken = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+         camera1 = Camera.open(0);
+         camera2 = Camera.open(1);
+         
+         SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surface1);
+         surfaceView.getHolder().addCallback(new CameraHandler(camera1, this));
+         
+
+    }
+    
+    public void PictureTaken() {
+    	if (pictureTaken)
+    		return;
+    	pictureTaken = true;
+        camera2 = Camera.open(1);
+        SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surface2);
+        surfaceView.getHolder().addCallback(new CameraHandler(camera2, this));
+       
     }
 
 
@@ -19,5 +47,6 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
     
 }
